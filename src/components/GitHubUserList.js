b88dedbdb89b50dd3405f95ea475bfc7c61e4034
rifadul <rifadul15-1886@diu.edu.bd>
2022-02-13 20:25:14 +0600

    fetch the data with axios from git api and showing all the data in front-end

--- src/components/GitHubUserList.js
+++ src/components/GitHubUserList.js
@@ -1,59 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function GitHubUserList() {
+    const [gitUsers, setGitUsers] = useState([]);
+
+    const getGitUser = async () => {
+        try {
+            const response = await axios.get('https://api.github.com/users');
+            setGitUsers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getGitUser();
+    }, []);
     return (
         <div>
             <h2>List of GitHub Users</h2>
             <div className="container-fluid mt-5">
                 <div className="row text-center">
-                    <div className="col-10 col-md-4 mt-5">
-                        <div className="card p-2">
-                            <div className="d-flex align-items-center">
-                                <div className="image">
-                                    <img
-                                        src="https://avatars.githubusercontent.com/u/46867681?v=4"
-                                        className="rounded"
-                                        width="155"
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="ml-3 w-100">
-                                    <h4 className="mb-0 mt-0 textLeft">
-                                        Rifadul Islam Khushnobish Siam
-                                    </h4>
-                                    <span className="text-left">Full Stack Web Developer</span>
-                                    <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
-                                        <div className="d-flex flex-column">
-                                            <span className="articles">
-                                                Articles
-                                            </span>{' '}
-                                            <span className="number1">
-                                                38
-                                            </span>{' '}
+                    {gitUsers.map((currentUser) => {
+                        const { html_url, avatar_url, id, login, type } =
+                            currentUser;
+                        return (
+                            <div className="col-10 col-md-4 mt-5" key={id}>
+                                <div className="card p-2">
+                                    <div className="d-flex align-items-center">
+                                        <div className="image">
+                                            <img
+                                                src={avatar_url}
+                                                className="rounded"
+                                                width="155"
+                                                alt=""
+                                            />
                                         </div>
-                                        <div className="d-flex flex-column">
-                                            <span className="followers">
-                                                Followers
-                                            </span>{' '}
-                                            <span className="number2">
-                                                980
-                                            </span>{' '}
-                                        </div>
-                                        <div className="d-flex flex-column">
-                                            <span className="rating">
-                                                Rating
-                                            </span>{' '}
-                                            <span className="number3">
-                                                8.9
-                                            </span>{' '}
+                                        <div className="ml-3 w-100">
+                                            <a href={html_url} className="text-decoration-none" target="_blank">
+                                                <h4 className="mb-0 mt-0 textLeft text-capitalize">
+                                                {login}
+                                            </h4>
+                                            </a>
+                                            
+                                            <span className="text-left">
+                                                {type}
+                                            </span>
+                                            <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                                                <div className="d-flex flex-column">
+                                                    <span className="articles">
+                                                        Articles
+                                                    </span>{' '}
+                                                    <span className="number1">
+                                                        38
+                                                    </span>{' '}
+                                                </div>
+                                                <div className="d-flex flex-column">
+                                                    <span className="followers">
+                                                        Followers
+                                                    </span>{' '}
+                                                    <span className="number2">
+                                                        980
+                                                    </span>{' '}
+                                                </div>
+                                                <div className="d-flex flex-column">
+                                                    <span className="rating">
+                                                        Rating
+                                                    </span>{' '}
+                                                    <span className="number3">
+                                                        8.9
+                                                    </span>{' '}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
-    )
+    );
 }
